e8834b5c3a1f9beb2aada640106d02402dbd909b
Ankit <anks.rane@gmail.com>
2024-08-27 18:00:35 +0530

    8th video complete

---
+++ 04bgchanger/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Testing</h1>
+      <button className="btn btn-success">Test</button>
+    </>
+  );
+}
+
+export default App;
