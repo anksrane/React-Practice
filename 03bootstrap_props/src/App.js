34dee4d17d86928b4203d9456a813486f6407c70
Ankit <anks.rane@gmail.com>
2024-08-27 16:27:43 +0530

    react basic complete

---
+++ 03bootstrap_props/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Cards from './components/Cards';
+
+function App() {
+  return (
+    <>
+    <Cards username='Ankit' btnText='Visit Here'/>
+    <Cards username='Ankit'/>
+    </>
+  );
+}
+
+export default App;
