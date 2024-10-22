c0c05a2d968166472c9d42416ad013eee292a3a5
Ankit <anks.rane@gmail.com>
2024-10-22 11:51:32 +0530

    Authentication service with appwrite completed

---
+++ 12Blogs_with_appwrite/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import conf from "./conf/conf.js";
+
+function App() {
+  console.log("appwriteBucketId",conf.appwriteBucketId);
+  return (
+    <>
+      <h1>A Blog with App Write</h1>
+    </>
+  )
+}
+
+export default App
