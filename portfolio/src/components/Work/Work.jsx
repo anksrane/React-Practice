f48de5f5d2e32943c45dd6db4062bea07cbfb88b
Ankit <anks.rane@gmail.com>
2025-01-28 18:22:34 +0530

    Journey Design Started

--- portfolio/src/components/Work/Work.jsx
+++ portfolio/src/components/Work/Work.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './Work.css';
+import React from "react";
+import img1 from "../../assets/image/g20.jpg";
+import img2 from "../../assets/image/vijay-tanks.jpg";
+import img3 from "../../assets/image/birla.jpg";
+import img4 from "../../assets/image/stockholding.jpg";
+import "./Work.css";
+
+const worklist = [
+  { src: img1, heading: 'G20', link: 'https://www.motherofdemocracyg20.com/' },
+  { src: img2, heading: 'Vijay Tanks', link: 'https://www.vijaytanks.com/' },
+  { src: img3, heading: 'Birla', link: 'https://www.birlaa1.com/birla-a1-locator-dealer.php' },
+  { src: img4, heading: 'Stockholding', link: 'https://www.stockholding.com/' },
+];
 
 function Work() {
   return (
-    <div className='work-container'>
-      <h1>WOrk</h1>
+    <div className="work-container">
+      <h1>Work</h1>
     </div>
-  )
+  );
 }
 
-export default Work
+export default Work;
