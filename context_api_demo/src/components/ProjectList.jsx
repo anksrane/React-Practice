e5651f53bfd58dc5512cdd6e08bca3de1cb34e41
Ankit <anks.rane@gmail.com>
2025-02-19 15:32:52 +0530

    context api completed

--- context_api_demo/src/components/ProjectList.jsx
+++ context_api_demo/src/components/ProjectList.jsx
@@ -2,19 +2,19 @@ import React, {useContext} from 'react';
 import { ProjectContext } from '../context/ProjectContext';
 
 function ProjectList() {
-  const {projects,setSelectedProjects}=useContext(ProjectContext);
+  const { projects, setSelectedProject } = useContext(ProjectContext); 
 
   return (
     <ul style={{listStyle:'none',padding:0,textAlign:"center"}}>
       {projects && projects.length > 0 ? 
-      projects.map((project)=>{
+      projects.map((project)=>(
         <li key={project.id}
-          onMouseEnter={()=>setSelectedProjects(project)}
+          onMouseEnter={()=>setSelectedProject(project)}
           style={{ padding: "10px", cursor: "pointer", fontSize: "18px", fontWeight: "bold" }}
         >
           <a href={project.anchor}>{project.projectName}</a>
         </li>
-      }):
+      )):
       (
         <li>No projects found</li>
       )}
