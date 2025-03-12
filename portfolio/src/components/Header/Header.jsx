90b15149e7fd4f5fabc04cc9e28c6d198801d4ad
Ankit <anks.rane@gmail.com>
2025-03-12 17:43:23 +0530

    bootstrap removed

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -5,23 +5,12 @@ import './Header.css'
 function Header() {
   return (
     <header>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+    <nav className="navbar">
+      <div className="nav-container">
         <Link className="navbar-brand" to="/">
           MyApp
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="navbar-collapse" id="navbar-tabs">
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to="/">
