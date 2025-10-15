f899b88d90dacaddee9f345fed7e25ebbde8cb33
anksrane <anks.rane@gmail.com>
2025-10-15 12:53:36 +0530

    Header Completed

--- portfolio-design/portfolio-v2/src/components/Header/Header.jsx
+++ portfolio-design/portfolio-v2/src/components/Header/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { toggleTheme } from "../../features/ui/themeSlice";
 import { NavLink } from "react-router-dom";
+
 import logo from "../../assets/images/AR-crop.png";
 import { IoSunny } from "react-icons/io5";
 import { MdDarkMode } from "react-icons/md";
 import { CgMenuRightAlt } from "react-icons/cg";
-import { HiOutlineMenu } from "react-icons/hi";
+import { IoCloseCircle } from "react-icons/io5";
+
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../../features/ui/themeSlice";
+import { toggleMenu } from "../../features/ui/mobMenuSlice";
 import "./Header.css";
 
 function Header() {
   const dispatch = useDispatch();
   const theme = useSelector((state) => state.theme.mode);  
+  const menuOpen = useSelector((state) => state.menuMobile.isMenuOpen);
   const navItems = [
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
@@ -31,7 +35,20 @@ function Header() {
             ></img>
           </NavLink>
 
-          <div className="nav-items">
+          <div className={`nav-items ${menuOpen? "open" : ""}`}>
+            <div className="mobile-head">
+              <NavLink to={"/"} className='logo-link logo-link-mobile'>
+                <img
+                  src={logo}
+                  alt="home"
+                  className="img-responsive logo-img"
+                ></img>
+              </NavLink>  
+              
+              <button className="close-mob-menu-btn" onClick={() => dispatch(toggleMenu())}>
+                <IoCloseCircle className="close-mob-menu-icon" />
+              </button>                             
+            </div>
             {navItems.map((item) => {
               return (
                 <NavLink
@@ -52,8 +69,8 @@ function Header() {
             >{theme === "dark" ? <MdDarkMode className="icon dark-icon"/> : <IoSunny className="icon light-icon"/>}
             </button>   
 
-            <button className="ham-btn">
-              <HiOutlineMenu className="ham-icon" />
+            <button className="ham-btn" onClick={() => dispatch(toggleMenu())}>
+              <CgMenuRightAlt className="ham-icon" />
             </button>                    
           </div>
         </div>
