534c21027d171a916677072698beea7dc1084b10
anksrane <anks.rane@gmail.com>
2026-01-21 11:43:18 +0530

    20 Days Files Moved to Full Stack Folder

--- day002-performance_in_real_apps/src/component/ChildCallbakcEg.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ChildCallbakcEg = React.memo(({ onClick }) => {
-  console.log("Child rendered");
-  return <button onClick={onClick}>Child Button</button>;
-});
-
-export default ChildCallbakcEg;
